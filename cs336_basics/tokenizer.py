4dae15af6c70b68af7ed1c270c4e4a42eed014dc
abbudjoe <abbudjoe@gmail.com>
2026-05-06 04:23:43 +0000

    Additional helper functions for tokenizer

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -103,15 +103,22 @@ class BPETokenizer:
     self.merges = merges
     self.special_tokens = special_tokens or []
 
-    def get_tokenizer(
-      vocab: dict[int, bytes],
-      merges: list[tuple[bytes, bytes]],
-      special_tokens: list[str] | None = None,
-    ) -> BPETokenizer:
-      return BPETokenizer(vocab, merges, special_tokens)
 
-    def decode(self, ids: list[int]) -> str:
-      token_bytes = b"".join(self.vocab[token_id] for token_id in ids)
-      return token_bytes.decode("utf-8", errors="replace")
+
+  def decode(self, ids: list[int]) -> str:
+    token_bytes = b"".join(self.vocab[token_id] for token_id in ids)
+    return token_bytes.decode("utf-8", errors="replace")
+  
+def get_tokenizer(
+  vocab: dict[int, bytes],
+  merges: list[tuple[bytes, bytes]],
+  special_tokens: list[str] | None = None,
+) -> BPETokenizer:
+  return BPETokenizer(vocab, merges, special_tokens)
+
+
     
+
+
+
 
