3a2b1e7863ab22920341079bfe728fbcc76a3764
abbudjoe <abbudjoe@gmail.com>
2026-05-13 04:50:24 +0000

    Additional helper functions for tokenizer

--- tests/adapters.py
+++ tests/adapters.py
@@ -8,7 +8,7 @@ import numpy.typing as npt
 import torch
 from jaxtyping import Bool, Float, Int
 from torch import Tensor
-
+from cs336_basics.model import linear
 
 def run_linear(
     d_in: int,
@@ -29,7 +29,7 @@ def run_linear(
         Float[Tensor, "... d_out"]: The transformed output of your linear module.
     """
 
-    raise NotImplementedError
+    return linear(d_in, d_out, in_features, weights)
 
 
 def run_embedding(
