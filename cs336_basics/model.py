7b64e05ce9f2fb8b8860e8bc67dd8f9b518c2b7b
abbudjoe <abbudjoe@gmail.com>
2026-05-15 04:25:54 +0000

    Additional helper functions for tokenizer

--- cs336_basics/model.py
+++ cs336_basics/model.py
@@ -4,26 +4,20 @@ import torch
 from torch import nn
 
 class Linear(nn.Module):
-  def __init__ (self, in_features: int, out_features: int):
+  def __init__ (self, d_in: int, d_out: int):
     super().__init__()
+    self.weight = nn.Parameter(torch.empty(d_out, d_in))
 
-    std = math.sqrt(2 / (in_features + out_features))
-    self.weight = nn.Parameter(
-      torch.empty(out_features, in_features)
-    )
-    nn.init.trunc_normal_(
-      self.weight,
-      mean = 0.0,
-      std = std,
-      a=-3 * std,
-      b=3 * std,
-    )
   def forward(self, x: torch.Tensor) -> torch.Tensor:
     return x @ self.weight.T
-  
-def linear(in_dim, out_dim, weights, in_features):
-  layer = Linear(in_dim, out_dim)
-  with torch.no_grad():
-    layer.weight.copy_(weights)
 
-  return layer(in_features)
+class Embedding(nn.Module):
+  def __init__(self, num_embeddings: int, embedding_dim: int):
+    super().__init__()
+    self.weight = nn.Parameter(torch.empty(num_embeddings, embedding_dim))
+
+  def forward(self, token_ids: torch.Tensor) -> torch.Tensor:
+    return self.weight[token_ids]
+
+class SwiGLU(nn.Module):
+  def __init__(self, d)
