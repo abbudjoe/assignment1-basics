f2373227adf1e74aee2446d57d3df5702585ff24
abbudjoe <abbudjoe@gmail.com>
2026-05-15 20:27:37 +0000

    Updates to adapters.py for new features

--- tests/adapters.py
+++ tests/adapters.py
@@ -11,6 +11,7 @@ from torch import Tensor
 from cs336_basics.model import Linear
 from cs336_basics.model import Embedding
 from cs336_basics.model import SwiGLU
+from cs336_basics.model import scaled_dot_product_attention
 
 def run_linear(
     d_in: int,
@@ -114,7 +115,7 @@ def run_scaled_dot_product_attention(
     Returns:
         Float[Tensor, " ... queries d_v"]: Output of SDPA
     """
-    raise NotImplementedError
+    return scaled_dot_product_attention(Q, K, V, mask)
 
 
 def run_multihead_self_attention(
